8d583afae77f9847445c771c9004dd96e1455cb0
JalukOne <jalukone@outlook.com>
2024-10-02 02:12:37 -0500

    Del

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "fibertracker",
   plugins: [
     remix({
       ssr: false,
-      basename: "/fibertracker/",
     }),
     tsconfigPaths(),
   ],
 });
-
